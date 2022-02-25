da2bb53397f1ee2c96ff8bdd42cea125046cd35f
Tony Holdstock-Brown <tonyhb@gmail.com>
2022-02-25 16:59:13 -0500

    Simplify entrypoint to reuse code

--- marshal.go
+++ marshal.go
@@ -1,8 +1,6 @@
 package cuetypescript
 
 import (
-	"fmt"
-
 	"cuelang.org/go/cue"
 	"github.com/inngest/cuetypescript/generation"
 )
@@ -10,12 +8,7 @@ import (
 // MarshalString marshals a Cue string into a Typescript type string,
 // returning an error.
 func MarshalString(cuestr string) (string, error) {
-	r := &cue.Runtime{}
-	inst, err := r.Compile(".", cuestr)
-	if err != nil {
-		return "", fmt.Errorf("error generating inst: %w", err)
-	}
-	return generation.MarshalCueValue(inst.Value())
+	return generation.MarshalString(cuestr)
 }
 
 // MarshalCueValue returns a typescript type given a cue value.
